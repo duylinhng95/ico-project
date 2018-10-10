a5fa266ee9a7e921e24a37e1c408b2824dc1167f
duylinhng95 <duylinhng95@gmail.com>
2018-10-10 19:03:54 +0700

    complete register login

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::get('/test', function () {
-    return view('admin/index');
+    return view('home');
 });
 
 Auth::routes();
