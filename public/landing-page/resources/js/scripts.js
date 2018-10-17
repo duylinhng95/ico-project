d38f7d5d0e89f1a8bc253408c7b65362366112fc
The Bao <thebao.dev@gmail.com>
2018-10-17 11:36:28 +0700

    Added landing-page-v0.1

---
+++ public/landing-page/resources/js/scripts.js
@@ -0,0 +1,79 @@
+$(document).ready(function() {
+  // /* Auto switch dark,light navigation */
+  // $(".js--whois-section").waypoint(
+  //   function(direction) {
+  //     if (direction == "down") {
+  //       $("nav").addClass("nav-bg-dark");
+  //     } else {
+  //       $("nav").removeClass("nav-bg-dark");
+  //     }
+  //   },
+  //   {
+  //     offset: "80px"
+  //   }
+  // );
+  // $(".js--features-section").waypoint(
+  //   function(direction) {
+  //     if (direction == "down") {
+  //       $("nav").addClass("nav-bg-white");
+  //       $(".nav-item > a").removeClass("nav-link");
+  //       $(".nav-item > a").addClass("nav-link-dark");
+  //     } else {
+  //       $("nav").removeClass("nav-bg-white");
+  //       $(".nav-item > a").addClass("nav-link");
+  //       $(".nav-item > a").removeClass("nav-link-dark");
+  //     }
+  //   },
+  //   {
+  //     offset: "80px"
+  //   }
+  // );
+  // $(".js--timeline-section").waypoint(
+  //   function(direction) {
+  //     if (direction == "down") {
+  //       $("nav").removeClass("nav-bg-white");
+  //       $(".nav-item > a").addClass("nav-link");
+  //       $(".nav-item > a").removeClass("nav-link-dark");
+  //     } else {
+  //       $("nav").addClass("nav-bg-white");
+  //       $(".nav-item > a").removeClass("nav-link");
+  //       $(".nav-item > a").addClass("nav-link-dark");
+  //     }
+  //   },
+  //   {
+  //     offset: "80px"
+  //   }
+  // );
+  // $(".js--advisor-section").waypoint(
+  //   function(direction) {
+  //     if (direction == "down") {
+  //       $("nav").addClass("nav-bg-white");
+  //       $(".nav-item > a").removeClass("nav-link");
+  //       $(".nav-item > a").addClass("nav-link-dark");
+  //     } else {
+  //       $("nav").removeClass("nav-bg-white");
+  //       $(".nav-item > a").addClass("nav-link");
+  //       $(".nav-item > a").removeClass("nav-link-dark");
+  //     }
+  //   },
+  //   {
+  //     offset: "80px"
+  //   }
+  // );
+  // $(".js--partners-section").waypoint(
+  //   function(direction) {
+  //     if (direction == "down") {
+  //       $("nav").removeClass("nav-bg-white");
+  //       $(".nav-item > a").addClass("nav-link");
+  //       $(".nav-item > a").removeClass("nav-link-dark");
+  //     } else {
+  //       $("nav").addClass("nav-bg-white");
+  //       $(".nav-item > a").removeClass("nav-link");
+  //       $(".nav-item > a").addClass("nav-link-dark");
+  //     }
+  //   },
+  //   {
+  //     offset: "80px"
+  //   }
+  // );
+});
