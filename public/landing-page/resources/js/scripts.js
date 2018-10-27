84c069b927755533a78bdb6564e6682399bafe03
Nguyen Duy Linh <32507314+duylinhng95@users.noreply.github.com>
2018-10-27 18:07:30 +0700

    Revert "7 fix bugs homepage"

--- public/landing-page/resources/js/scripts.js
+++ public/landing-page/resources/js/scripts.js
@@ -1,35 +1,36 @@
 (function($) {
   "use strict";
-  //Allows bootstrap carousels to display 3 items per page rather than just one
-  $(".carousel.carousel-multi .item").each(function() {
-    if ($(window).width() >= 992) {
-      var next = $(this).next();
-      if (!next.length) {
-        next = $(this).siblings(":first");
-      }
-      next
-        .children(":first-child")
-        .clone()
-        .attr("aria-hidden", "true")
-        .appendTo($(this));
 
-      if (next.next().length > 0) {
-        next
-          .next()
-          .children(":first-child")
-          .clone()
-          .attr("aria-hidden", "true")
-          .appendTo($(this));
-      } else {
-        $(this)
-          .siblings(":first")
-          .children(":first-child")
-          .clone()
-          .appendTo($(this));
+  // Control buttons
+  $("#myCarousel .carousel-control-next").click(function() {
+    $("#myCarousel").carousel("next");
+    return false;
+  });
+  $("#myCarousel .carousel-control-prev").click(function() {
+    $(" #myCarousel").carousel("prev");
+    return false;
+  });
+
+  // On carousel scroll
+  $("#myCarousel").on("slide.bs.carousel", function(e) {
+    var $e = $(e.relatedTarget);
+    var idx = $e.index();
+    var itemsPerSlide = 3;
+    var totalItems = $("#myCarousel .carousel-item").length;
+    if (idx >= totalItems - (itemsPerSlide - 1)) {
+      var it = itemsPerSlide - (totalItems - idx);
+      for (var i = 0; i < it; i++) {
+        // append slides to end
+        if (e.direction == "left") {
+          $(" #myCarousel .carousel-item")
+            .eq(i)
+            .appendTo(" #myCarousel .carousel-inner");
+        } else {
+          $("#myCarousel .carousel-item")
+            .eq(0)
+            .appendTo("#myCarousel .carousel-inner");
+        }
       }
-    } else {
-      $(".left .carousel-control").addClass("carousel-control-prev");
-      $(".right .carousel-control").addClass("carousel-control-next");
     }
   });
 })(jQuery);
