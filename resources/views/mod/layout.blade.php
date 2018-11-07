396673b7d4be0960bf8f232234ce4f5afb4cbe54
linhsfr <duylinhng95@gmail.com>
2018-11-07 15:54:24 +0700

    change icon mod

--- resources/views/mod/layout.blade.php
+++ resources/views/mod/layout.blade.php
@@ -61,7 +61,7 @@
                         <li>
                             <a class="nav-link" href="{{ route('logout') }}"
                                                                 onclick="event.preventDefault();
-                                                                document.getElementById('logout-form').submit();"><i class="fas fa-sign-out-alt fa-fw"></i> Logout</a>
+                                                                document.getElementById('logout-form').submit();"><i class="fa fa-sign-out-alt fa-fw"></i> Logout</a>
                               <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                                                                 {{ csrf_field() }}
                                                             </form>
